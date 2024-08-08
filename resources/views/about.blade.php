445108e3c7d353abc662487ef57717d511ab04ec
Xu Chunyang <xuchunyang56@gmail.com>
2024-08-08 11:57:59 +0800

    新窗口打开链接

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,7 +8,7 @@
 <h1>关于</h1>
 <p>
     这是用 NativePHP 搭建的菜单栏小工具，用来查看奥运奖牌排行榜，数据来自 Wikipedia。访问 <a
-        href="https://github.com/xuchunyang/olympics-medal-nativephp">GitHub</a> 查看源代码。
+        href="https://github.com/xuchunyang/olympics-medal-nativephp" target="_blank">GitHub</a> 查看源代码。
 </p>
 </body>
 </html>
